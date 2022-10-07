3f348412aa1a60d16b22d22bcad80b8210dcf663
May Carandang <maytiara@gmail.com>
2022-10-07 17:52:25 +0800

    mern stack initialize

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello There 🧑‍🚀</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
