e25aa3519d1c1794de292e3716199d516d749119
May Carandang <maytiara@gmail.com>
2022-10-12 02:01:12 +0800

    modified models syntax with some additional property value

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -1,39 +1,39 @@
-const { Schema, model } = require('mongoose');
-const User = require('./User');
+const { Schema, model } = require("mongoose");
+const User = require("./User");
 
-const reservationSchema = new Schema({
-  numOfPeople: {
-    type: Number, // because its an integer
-    required: true,
-  },
-  defineExp: {
-    type: String,
-    required: 'Must explain your fine dining experience',
-    minlength: 1, // user can type paragraph of this section
-    trim: true,
-  },
-  budget: {
-    type: String, // for dropdown selection
-    required: true,
-  },
-  dietary: {
-    type: String, // for dropdown selection
-  },
-  chefId: {
-    type: Schema.Types.ObjectId,
-    ref: User,
-  },
-  eventDate: {
-    type: Date,
-    require: true,
-  }
-},
-
-  {
-    timestamps: true, //this createdAt
-  }
+const reservationSchema = new Schema(
+	{
+		numOfPeople: {
+			type: Number, // because its an integer
+			required: true,
+		},
+		defineExp: {
+			type: String,
+			required: "Must explain your fine dining experience",
+			minlength: 1, // user can type paragraph of this section
+			trim: true,
+		},
+		budget: {
+			type: String, // for dropdown selection
+			required: true,
+		},
+		dietary: {
+			type: String, // for dropdown selection
+		},
+		chefId: {
+			type: Schema.Types.ObjectId,
+			ref: User,
+		},
+		eventDate: {
+			type: Date,
+			require: true,
+		},
+	},
+	{
+		timestamps: true, //this createdAt
+	},
 );
 
-const Reservation = mongoose.model('Reservation', reservationSchema);
+const Reservation = mongoose.model("Reservation", reservationSchema);
 
 module.exports = Reservation;
--- server/models/User.js
+++ server/models/User.js
@@ -1,65 +1,65 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-  fullname: {
-    
-    // user may have the same name
-    type: String,
-    required: true,
-    trim: true,
-  },
-  address: {
-    // user may have the same add
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-    trim: true,
-  },
-  company: {
-    // user may work in the same company & this could be null
-    type: String,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  isChef: {
-    type:Boolean,
-    default: false,
-    // findAll --> isTrue ; user || chef role
-  }, 
-  reservations: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Reservation',
-    },
-  ]
+	fullname: {
+		// user may have the same name
+		type: String,
+		required: true,
+		trim: true,
+	},
+	address: {
+		// user may have the same add
+		type: String,
+		required: true,
+		minlength: 1,
+		maxlength: 280,
+		trim: true,
+	},
+	company: {
+		// user may work in the same company & this could be null
+		type: String,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+    trim: true, //to avoid spacing from start and end of the string
+		match: [/.+@.+\..+/, "Must match an email address!"],
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 5,
+	},
+	isChef: {
+		type: Boolean,
+		default: false,
+		// findAll --> isTrue ; user || chef role
+	},
+	reservations: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Reservation",
+		},
+	],
 });
 
 // compare the incoming password with the hashed password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
 
-  next();
+	next();
 });
 
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+	return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
