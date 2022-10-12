d41028c76f4da9e704c7fc5bef2da314725251cf
May Carandang <maytiara@gmail.com>
2022-10-12 19:56:09 +0800

    defined Mutation functionalities for resolvers schemas by matching the context & arguments in typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,9 +16,20 @@ const resolvers = {
     user: async (parent, {username}) => {
       return User.findOne({ username }).populate('reservations');
     },
-    //chef(userId: ID!): User
-    chef: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
+
+    // --check this one carefully
+    //chefs(userId: ID!): User
+    chefs: async (parent, { id }, context) => {
+      if (context.user) {
+        const isChef = await User.findAll(context.user.id).populate({
+          path: 'users.reservations',
+          populate: 'isChef',
+        });
+
+        return isChef.users.id(id);
+      }
+
+      throw new AuthenticationError ('Not logged in');
     },
     //reservations(chefId: ID!): [Reservation]
     reservations: async (parent, { chefId }) => {
@@ -34,10 +45,67 @@ const resolvers = {
         return User.findOne({ _id: context.user._id }).populate ('reservations');
       }
       throw new AuthenticationError('Sorry, Please logged in');
-    }
+    },
+  },
 
+  // this have to match the typeDefs Mutation
+  Mutation: {
+    
+    // getting all the User fields
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // Mutation: typeDefs
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true});
+      }
+      throw new AuthenticationError('Sorry, Please logged in');
+    },
+    // Mutation: typeDefs
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // Mutation: typeDefs
+    addReservation: async (parent, { reservations }, context) => {
+      console.log(context);
+      if (context.user) {
+        const reservation = new Reservation({ reservations });
+
+        await Reservation.findByIdAndUpdate(context.user._id, { $push: { reservations: reservation } });
+
+        return reservation;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    // Mutation: typeDefs
+    updateReservation: async (parent, args, context) => {
+      if (context.reservation) {
+        return await Reservation.findByIdAndUpdate(context.reservation._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    }
   }
 
-}
+};
 
 module.exports = resolvers;
