d46b05d2e855d286d813151512e4e817f11ebb55
May Carandang <maytiara@gmail.com>
2022-10-13 01:29:23 +0800

    added Home for main page and imported the component with css

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import css from './Home.module.css'; //css
+
+import Hero from '../components/Hero/Hero'; //components
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <header className=''>
+      </header>
+    </div>
+  )
+}
+
+export default Home
