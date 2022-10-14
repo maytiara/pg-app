f501e82ae33c17272a30abba88b9b9bb522003e3
May Carandang <maytiara@gmail.com>
2022-10-14 16:13:53 +0800

    added files to seed the users & reservations collection. Populated the reservations field in users.

---
+++ server/seeders/seed.js
@@ -0,0 +1,53 @@
+const db = require('../config/connection');
+const { User, Reservation } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const reservationSeeds = require('./reservationSeeds.json');
+
+function randomArrayElement(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+db.once('open', async () => {
+  try {
+    await Reservation.deleteMany({});
+    await User.deleteMany({});
+
+
+    // seed the user table
+    const users = await User.insertMany(userSeeds);
+
+
+    // grab the chefs
+    const chefs = await User.find({isChef: true});
+
+
+    // seed the reservation table
+    for (let i = 0; i < reservationSeeds.length; i++) {
+      const { _id, reservation} = await Reservation.create({
+        ...reservationSeeds[i],
+        chefId: randomArrayElement(chefs)._id
+      });
+
+      // randomly assign the reservation to customers
+      const customers = users.filter(user => !user.isChef);
+      const randomCustomer = randomArrayElement(customers);
+
+      await User.findOneAndUpdate({ //removed ._id after randomCustomer
+
+        randomCustomer: reservation }, // to populate the reservations field in users
+      {
+        $addToSet: {
+          reservations: _id,
+        },
+      }, {new: true});
+
+    
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('Well Done! You planted some seeds 🌱');
+  process.exit(0);
+});
