0a98a8d970af42fefa82e91d642fb8a9e7d9d79a
May Carandang <maytiara@gmail.com>
2022-10-17 05:01:13 +0800

    have to use the lower version of mongoose which is not latest than ^v6. To be able to use the following properties: useCreateIndex and useFindAndModify

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pg-app-v1',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
   
 module.exports = mongoose.connection;
