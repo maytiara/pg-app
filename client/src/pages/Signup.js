ce02af67695a11d71459d2e2c07c3d5f0d90af02
May Carandang <maytiara@gmail.com>
2022-10-17 04:13:43 +0800

    modified create account page by adding the textfields, different hooks, using the mutation ADD_USER for the new account and also, modified the material ui customised component.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,185 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
+import css from "./Signup.module.css";
+import { Container, Box, Paper, TextField, Typography } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../styles/Theme';
 
-function Signup () {
-  return (
-    <div>Signup</div>
-  )
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/Buttons/NavBar";
+import { RoundedButton } from "../styles/StyledButton";
+
+function Signup(props) {
+	const [formState, setFormState] = useState({ 
+    username: "",
+    email: "",
+    password: "",
+    company: "",
+    address: ""
+  });
+	const [addUser] = useMutation(ADD_USER);
+
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+		const mutationResponse = await addUser({
+			variables: {
+				username: formState.username,
+				email: formState.email,
+				password: formState.password,
+				company: formState.company,
+				address: formState.address,
+				// isChef
+			},
+		});
+		const token = mutationResponse.data.addUser.token;
+		Auth.login(token);
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	};
+
+	return (
+		<div
+			style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+			className={css.heroImg}
+		>
+			<NavBar />
+
+			<Container
+				sx={{
+					minwidth: "20vh",
+					maxWidth: "40vh",
+					display: "flex",
+					justifyContent: "center",
+					direction: "column",
+				}}
+			>
+				<Paper
+					sx={{
+						marginTop: 2.5,
+						padding: "1rem",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Box
+						component="form"
+						onSubmit={handleFormSubmit}
+						noValidate
+						sx={{ "& > :not(style)": { m: 1, width: "35ch" } }}
+						autoComplete="off"
+						display="inline"
+						justifyContent="center"
+						maxWidth="50vh"
+					>
+						{/* Username */}
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="username"
+							label="First Name & Last Name"
+							type="username"
+							name="username"
+							helperText="This field is required"
+							variant="standard"
+							sx={{ width: "15rem" }}
+							onChange={handleChange}
+						/>
+						<></>
+						{/* Email address */}
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							type="email"
+							name="email"
+							helperText="This field is required"
+							variant="standard"
+							sx={{ width: "15rem" }}
+							onChange={handleChange}
+						/>
+
+						{/* Password */}
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="password"
+							label="Password"
+							type="password"
+							name="password"
+							helperText="This field is required"
+							variant="standard"
+							sx={{ width: "15rem" }}
+							onChange={handleChange}
+						/>
+
+						{/* Password */}
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="company"
+							label="Company"
+							type="company"
+							name="company"
+							variant="standard"
+							sx={{ width: "15rem" }}
+							onChange={handleChange}
+						/>
+
+						{/* Password */}
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="address"
+							label="Address"
+							type="address"
+							name="address"
+							helperText="This field is required"
+							variant="standard"
+							sx={{ width: "15rem" }}
+							onChange={handleChange}
+						/>
+
+						<ThemeProvider theme={theme}>
+							<RoundedButton
+								color="secondary"
+								variant="contained"
+								sx={{ opacity: "90%" }}
+							>
+								<Typography
+									sx={{ fontSize: "1.15rem", fontWeight: 300, textTransform: "none" }}
+								>
+									<Link style={{ textDecoration: "none", color: "#0c0c0c" }}>
+										Create Account
+									</Link>
+								</Typography>
+							</RoundedButton>
+						</ThemeProvider>
+					</Box>
+				</Paper>
+			</Container>
+
+			<Box components="footer" sx={{ mt: "auto" }}>
+				<Footer />
+			</Box>
+		</div>
+	);
 }
-  
 
 export default Signup;
