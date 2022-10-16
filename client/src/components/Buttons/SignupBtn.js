8c41c1eca9f645b795c773cb4facb92e1eb2f604
May Carandang <maytiara@gmail.com>
2022-10-16 20:52:00 +0800

    added sign in button & create account button and used material UI

--- client/src/components/Buttons/SignupBtn.js
+++ client/src/components/Buttons/SignupBtn.js
@@ -2,20 +2,19 @@ import React from 'react'
 import theme from '../../styles/Theme';
 import { RoundedButton } from '../../styles/StyledButton';
 
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import { Stack, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
 
-const LoginButton = () => {
+const SignupButton = () => {
   return (
     <ThemeProvider theme={theme}>
       <Stack sx={{ width: '300px', height: '2rem'}}>
-        <RoundedButton color="secondary" variant="contained" sx={{ opacity: "90%" }}>
+        <RoundedButton color="secondary" variant="contained" sx={{ opacity: "90%"}}>
           <Typography sx={{ fontSize: '1.15rem', fontWeight: 300, textTransform: "none" }}>
-            <Link to="/login" style={{ textDecoration: 'none', color:  '#0c0c0c' }} >
-            Sign in
+            <Link to="/signup" style={{ textDecoration: 'none', color: '#0c0c0c' }} >
+            Create account
             </Link>
           </Typography>
         </RoundedButton>
@@ -25,4 +24,4 @@ const LoginButton = () => {
 }
 
 
-export default LoginButton
+export default SignupButton
