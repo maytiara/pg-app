5ad1563cf4ab6294f4c4dbb9c82eca315a17860d
May Carandang <maytiara@gmail.com>
2022-10-19 03:58:43 +0800

    added hard coded data for the diff chef components

--- client/src/components/Chefs/ChefAxelY.js
+++ client/src/components/Chefs/ChefAxelY.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import theme from "../../styles/Theme";
+import { ThemeProvider } from "@mui/material/styles";
 
-const ChefAxelY = () => {
-  return (
-    <div>ChefAxelY</div>
-  )
+//import ChefA from "../components/isChef/ChefA";
+
+import {
+	Container,
+	Card,
+	CardContent,
+	CardMedia,
+	Typography,
+	Button,
+	CardActionArea,
+	CardActions,
+} from "@mui/material";
+
+import { useParams } from "react-router-dom";
+import NavBar from '../Buttons/NavBar';
+
+export default function ChefAxelYu() {
+
+  const { username } = useParams();
+
+	return (
+		<div>
+			<NavBar />
+			<ThemeProvider theme={theme}>
+			<Container>
+				<Card sx={{ maxWidth: 345 }}>
+					<CardActionArea >
+						<CardMedia
+							component="img"
+							height="300px"
+							image="/static/images/cards/contemplative-reptile.jpg"
+							alt="Chef Axel Yu"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="h5"
+								color="secondary.main"
+								textAlign="left"
+							>
+								{ username }
+							</Typography>
+							<Typography variant="body2" color="primary.dark" textAlign="left">
+								Services: Private Chef, Degustation, Corporate & Social Events
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+
+					<CardActions>
+						<Button size="small" color="primary">
+							VIEW DETAILS
+						</Button>
+					</CardActions>
+				</Card>
+			</Container>
+		</ThemeProvider>
+		</div>
+		
+	);
+
+
 }
-
-export default ChefAxelY
--- client/src/components/Chefs/ChefMoriahM.js
+++ client/src/components/Chefs/ChefMoriahM.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import theme from "../../styles/Theme";
+import { ThemeProvider } from "@mui/material/styles";
 
-const ChefMoriahM = () => {
-  return (
-    <div>ChefMoriahM</div>
-  )
+//import ChefA from "../components/isChef/ChefA";
+
+import {
+	Container,
+	Card,
+	CardContent,
+	CardMedia,
+	Typography,
+	Button,
+	CardActionArea,
+	CardActions,
+} from "@mui/material";
+
+import { useParams } from "react-router-dom";
+
+export default function ChefMoriahMcgrath(children) {
+
+  const { username } = useParams();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container>
+				<Card sx={{ maxWidth: 345 }}>
+					<CardActionArea >
+						<CardMedia
+							component="img"
+							height="300px"
+							image="/static/images/cards/contemplative-reptile.jpg"
+							alt="Chef Moriah Mcgrath"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="h5"
+								color="secondary.main"
+								textAlign="left"
+							>
+								{ username }
+							</Typography>
+							<Typography variant="body2" color="primary.dark" textAlign="left">
+								Services: Private Chef, Degustation, Corporate & Social Events
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+
+					<CardActions>
+						<Button size="small" color="primary">
+							VIEW DETAILS
+						</Button>
+					</CardActions>
+				</Card>
+			</Container>
+		</ThemeProvider>
+	);
 }
-
-export default ChefMoriahM
