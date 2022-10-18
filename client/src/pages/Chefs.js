35b59d375a366a7b1553b92ac619c6a32c52153a
May Carandang <maytiara@gmail.com>
2022-10-19 02:49:32 +0800

    modified chefs component to link the different chefs id

--- client/src/pages/Chefs.js
+++ client/src/pages/Chefs.js
@@ -1,32 +1,61 @@
 import React from "react";
 import NavBar from "../components/Buttons/NavBar";
 import SearchField from "../components/Search/SearchField";
-import ChefsDetails from "./ChefDetails";
+import Footer from "../components/Footer/Footer";
 
 import Auth from '../utils/auth';
+import { Box } from "@mui/material";
 
-function Chefs(props) {
+import { Link } from "react-router-dom";
+
+function Chefs() {
+
+	const url = "/chefs";
+
 	return (
-		<>
-			
+		<div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
 			<NavBar />
 			<SearchField />
 			<div>
 				{Auth.loggedIn() ? (
 					<>
-					<span>
-						<p> Welcome {Auth.getProfile().data.username} </p>
-					</span>
+						<span>
+							<p> Welcome {Auth.getProfile().data.username} </p>
+						</span>
 					</>
-					) :(
-						<>
-							<p> You need to be logged in to make a booking. </p>
-						</>
-					)}
+				) : (
+					<>
+						<p> You need to be logged in to make a booking. </p>
+					</>
+				)}
 			</div>
 			<div style={{ marginTop: 50 }}></div>
-			<ChefsDetails />
-		</>
+
+			<div>
+				<Link to={`${url}/chef-axel-yu`}>hfgh</Link>
+			</div>
+			<div>
+				<Link to={`${url}/chef-moriah-mcgrath`}>ghgxg</Link>
+			</div>
+			<div>
+				<Link to={`${url}/chef-nancy-castillo`}>fjhj</Link>
+			</div>
+			<div>
+				<Link to={`${url}/chef-oli-buenviaje`}>fdhgf</Link>
+			</div>
+			<div>
+				<Link to={`${url}/chef-westin-pace`}>fghf</Link>
+			</div>
+
+			<Box
+				components="footer"
+				sx={{
+					mt: "auto",
+				}}
+			>
+				<Footer />
+			</Box>
+		</div>
 	);
 }
 
