5a034f7264224baeb069ac3d52d13af61ee80336
May Carandang <maytiara@gmail.com>
2022-10-18 16:59:17 +0800

    added authentication for the registered and non-registered user.

--- client/src/pages/Chefs.js
+++ client/src/pages/Chefs.js
@@ -1,11 +1,31 @@
 import React from "react";
 import NavBar from "../components/Buttons/NavBar";
+import SearchField from "../components/Search/SearchField";
+import ChefsDetails from "./ChefDetails";
 
-function Chefs() {
+import Auth from '../utils/auth';
+
+function Chefs(props) {
 	return (
 		<>
+			
 			<NavBar />
-			<div>Chefs</div>
+			<SearchField />
+			<div>
+				{Auth.loggedIn() ? (
+					<>
+					<span>
+						<p> Welcome {Auth.getProfile().data.username} </p>
+					</span>
+					</>
+					) :(
+						<>
+							<p> You need to be logged in to make a booking. </p>
+						</>
+					)}
+			</div>
+			<div style={{ marginTop: 50 }}></div>
+			<ChefsDetails />
 		</>
 	);
 }
